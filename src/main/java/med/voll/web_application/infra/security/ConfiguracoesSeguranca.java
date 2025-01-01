f62627c7d56121d36ee527cef79d601d5edaa8fe
milene <milenefaria33@gmail.com>
2025-01-01 17:06:32 -0300

    feat: criacao de paginas de login e logout

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,11 +2,13 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -15,9 +17,25 @@ public class ConfiguracoesSeguranca {
     @Bean
     public UserDetailsService dadosUsuariosCadastrados() {
         UserDetails usuario = User.builder()
-                .username("maria")
+                .username("maria@gmail.com")
                 .password("{noop}maria123").build();
 
         return new InMemoryUserDetailsManager(usuario);
+    }
+
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(req -> {
+                        req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+                        req.anyRequest().authenticated();
+                })
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login?logout")
+                        .permitAll())
+                .build();
     }
 }
